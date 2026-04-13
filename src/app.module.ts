6b338950b2cd621fff1f66744cb8cbbdb83891dc
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2026-04-13 11:07:16 -0500

    backend base arquitecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './modules/health/health.module';
+import { AuthIntegrationModule } from './modules/auth-integration/auth-integration.module';
+import { SessionsModule } from './modules/sessions/sessions.module';
+import { CollaborationModule } from './modules/collaboration/collaboration.module';
+import { ExecutionModule } from './modules/execution/execution.module';
+import { RedisModule } from './modules/redis/redis.module';
+import { PersistenceModule } from './modules/persistence/persistence.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+    }),
+    HealthModule,
+    AuthIntegrationModule,
+    SessionsModule,
+    CollaborationModule,
+    ExecutionModule,
+    RedisModule,
+    PersistenceModule,
+  ],
 })
 export class AppModule {}
